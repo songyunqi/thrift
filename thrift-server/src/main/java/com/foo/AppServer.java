c8f95ec515e2260f380728515e7e5765cde2c524
宋运旗 <songyunqi@qianmi.com>
2017-09-26 10:56:00 +0800

    xxx

--- thrift-server/src/main/java/com/foo/AppServer.java
+++ thrift-server/src/main/java/com/foo/AppServer.java
@@ -3,13 +3,14 @@ package com.foo;
 import com.foo.thrift.ProfileService;
 import com.foo.thrift.ProfileServiceImpl;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.thrift.TMultiplexedProcessor;
 import org.apache.thrift.TProcessorFactory;
 import org.apache.thrift.protocol.TCompactProtocol;
 import org.apache.thrift.server.THsHaServer;
 import org.apache.thrift.server.TServer;
-import org.apache.thrift.transport.TFramedTransport;
-import org.apache.thrift.transport.TNonblockingServerSocket;
-import org.apache.thrift.transport.TTransportException;
+import org.apache.thrift.server.TSimpleServer;
+import org.apache.thrift.server.TThreadPoolServer;
+import org.apache.thrift.transport.*;
 
 @Slf4j
 public class AppServer {
@@ -37,6 +38,25 @@ public class AppServer {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+
+    public void go() {
+        try {
+            TMultiplexedProcessor processor = new TMultiplexedProcessor();
+            TServerTransport t = new TServerSocket(port);
+            TServer server = new TThreadPoolServer(new TThreadPoolServer.Args(t).processor(processor));
+            //processor.registerProcessor("TopicService", new TopicService.Processor<TopicService.Iface>(new TopicImpl()));
+            processor.registerProcessor("UserService", new ProfileService.Processor<ProfileService.Iface>(new ProfileServiceImpl()));
+            //TSimpleServer server = new TSimpleServer(new THsHaServer.Args(t).processor(processor));
+            System.out.println("the serveris started and is listening at 9090...");
+            server.serve();
+        } catch (TTransportException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 
     public static void main(String[] args) {
